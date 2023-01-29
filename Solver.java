9e2f93b867b43434d854216a45beefafb81e50bf
Xuange_Aha <108124782+Xuangeaha@users.noreply.github.com>
2023-01-29 09:59:30 +0800

    2023.2.2

--- Solver.java
+++ Solver.java
@@ -3,7 +3,7 @@ import java.util.InputMismatchException;
 
 public class Solver {
     public static void main(String[] args) {
-        System.out.println("一元二次方程计算器 Linear Equation with Two Unknowns Solver 2023.2.1");
+        System.out.println("一元二次方程计算器 Linear Equation with Two Unknowns Solver 2023.2.2");
         System.out.println("作者/开发者：轩哥啊哈OvO");
         System.out.println("Copyright (c) 2023 轩哥啊哈OvO");
         System.out.println("----------------------------------------------------------------------");
@@ -14,20 +14,20 @@ public class Solver {
 
         var scanner = new Scanner(System.in);
         try {
-            System.out.println("  ??????a?????");
+            System.out.println("  请设置a的值：");
             a = scanner.nextDouble();
         } catch (InputMismatchException e) {
-            raise_solution("a???????????????????????");
+            raise_solution("a的值输入有误，请重新输入。");
         }
 
         try {
-            System.out.println("  ??????b?????");
+            System.out.println("  请设置b的值：");
             b = scanner.nextDouble();
         } catch (InputMismatchException e) {
-            raise_solution("b???????????????????????");
+            raise_solution("b的值输入有误，请重新输入。");
         }
 
-        System.out.println("  ??????c?????");
+        System.out.println("  请设置c的值：");
         scanner.nextLine();
         String cget = scanner.nextLine();
         System.out.println(cget);
@@ -41,26 +41,26 @@ public class Solver {
             if (b == 0) {
                 if (c == 0) {
                     equation_to_solve("0=0");
-                    raise_solution("???????????????");
+                    raise_solution("原方程的解为任意实数");
                 } else {
                     equation_to_solve(c+"=0");
-                    raise_solution("????????");
+                    raise_solution("原方程无解");
                 }
             } else {
                 equation_to_solve(b+"x+"+c+"=0");
                 double answer = (-1 * c) / b;
-                raise_solution("????x="+answer);
+                raise_solution("解得：x="+answer);
             }
         } else {
             equation_to_solve(a+"x2+"+b+"x+"+c+"=0");
             double delta = b * b - 4 * a * c;
             double delta_sqrt = Math.sqrt(delta);
             if (Double.isNaN(delta_sqrt)) {
-                raise_solution("????????????");
+                raise_solution("原方程无实数根");
             } else {
                 double x1 = (-1 * b + delta) / 2 * a;
                 double x2 = (-1 * b - delta) / 2 * a;
-                raise_solution("????x1="+x1+", x2="+x2);
+                raise_solution("解得：x1="+x1+", x2="+x2);
             }
         }
     }
