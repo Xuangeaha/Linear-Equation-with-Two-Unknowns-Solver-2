ff4adc65c265f96c24fbf255ff2c167cb55ae759
Xuange_Aha <108124782+Xuangeaha@users.noreply.github.com>
2023-01-29 10:16:11 +0800

    2022.3.1

--- out/production/Linear Equation with Two Unknowns Solver 2/README.md
+++
@@ -1,2 +0,0 @@
-# 轩氏一元二次方程计算器 Linear Equation with Two Unknowns Solver 2
-Copyright (c) 2023 轩哥啊哈OvO
--- out/production/Linear Equation with Two Unknowns Solver 2/solver.py
+++
@@ -1,6 +0,0 @@
-import jpype
-
-jvmPath = jpype.getDefaultJVMPath()
-jpype.startJVM(jvmPath)
-
-jpype.java.lang.System.out.println("123")
